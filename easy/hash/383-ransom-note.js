511564fbd2280217d74b2abae08fe287164d40b1
Dimitra Prousalis <dimitra.o.p@gmail.com>
2024-02-15 15:14:29 -0800

    ransom note

---
+++ easy/hash/383-ransom-note.js
@@ -0,0 +1,50 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+// Example 1:
+
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+// Example 2:
+
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= ransomNote.length, magazine.length <= 105
+// ransomNote and magazine consist of lowercase English letters.
+
+
+
+//Solution
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+const canConstruct = (ransomNote, magazine) => {
+    const map = new Map();
+
+    for (const char of magazine) {
+        map.set(char, (map.get(char) || 0) + 1);
+    }
+
+    for (const char of ransomNote) {
+        if (!map.has(char) || map.get(char) === 0) {
+            return false;
+        }
+        map.set(char, map.get(char) - 1);
+    }
+
+    return true;
+};
